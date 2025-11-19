d7c1593d3fba1495cac3299b0bdd6a3a6fea7305
jihyeon1011 <jihyeon1619@gmail.com>
2025-11-19 13:34:46 +0900

    submit

--- backend/api/src/main/java/com/example/onbid/dto/Wishlist.java
+++ backend/api/src/main/java/com/example/onbid/dto/Wishlist.java
@@ -1,10 +1,12 @@
 package com.example.onbid.dto;
 
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Data
 public class Wishlist {
     private Long id;
     private String username;
     private String propertyId;
+    private LocalDateTime createdAt;
 }
